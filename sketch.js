3d20802320a04786225e401c4f84e0dcb83d8b56
Wendy <cv47522@gmail.com>
2020-04-01 14:40:33 +0300

    Changed to plain JS version

--- sketch.js
+++ sketch.js
@@ -1,41 +1,79 @@
+/* ===
+ml5 Example
+Simple Image Classification using MobileNet
+This example uses promises to create the classifier
+=== */
+
+const image = document.getElementById('image');
+const result = document.getElementById('result');
+const probability = document.getElementById('probability');
+
 // Initialize the Image Classifier method with MobileNet
+ml5.imageClassifier('MobileNet')
+  .then(classifier => classifier.classify(image))
+  .then(results => {
+    result.innerText = results[1].label;
+    probability.innerText = results[1].confidence.toFixed(4);
+  })
 
-let classifier;
-let puffin;
 
-function preload() {
-  classifier = ml5.imageClassifier('MobileNet', modelReady);
-  puffin = loadImage('images/puffin.jpg', imageReady);
+
+//------------------------ How Promise Object works ------------------
+// Example 1
+let p = new Promise((fulfillFunc, rejectFunc) => {
+  let equation = ((1 + 2) == 2) ? fulfillFunc('Sucess') : rejectFunc('Failed');
+});
+
+// p.then(/* The result of equation corresponds to "fulfillFunc" */).catch(/* ...corresponds to "rejectFunc" */)
+p.then(message => {
+  console.log('This is in the "then": ' + message);
+}).catch(message => {
+  console.log('This is in the "catch": ' + message);
+});
+
+// Example 2
+let userLeft = true;
+let userWatchingCatMeme = false;
+
+function watchTVPromise() {
+  return new Promise((fulfillFunc, rejectFunc) => {
+    if (userLeft) {
+    	rejectFunc({
+      	name: 'User Left',
+      	message: ':(',
+    	});
+    }else if (userWatchingCatMeme) {
+      rejectFunc({
+        name: 'User Watching Cat Meme',
+      	message: ':)',
+      });
+    }else {
+      fulfillFunc('What are you watching?');
+    }
+  })
 }
 
-function setup() {
-  createCanvas(640, 480);
-  // Make a prediction with a selected image
-  classifier.classify(puffin, gotResults);
-  image(puffin, 0, 0, width, height);
-}
+watchTVPromise().then(message => {
+	console.log('Sucess: ' + message);
+}).catch(error => {
+	console.log(error.name + ' ' + error.message);
+});
 
-// Custom Functions
-function imageReady() {
-  console.log('Image is ready!');
-}
+// Example 3
+const recordVideoOne = new Promise((fulfill, reject) => {
+  fulfill('Video 1 Recorded');
+});
+const recordVideoTwo = new Promise((fulfill, reject) => {
+  fulfill('Video 2 Recorded');
+});
+const recordVideoThree = new Promise((fulfill, reject) => {
+  fulfill('Video 3 Recorded');
+});
 
-function modelReady() {
-  console.log('Model Loaded!');
-}
-
-function gotResults(error, results) {
-  if(error) {
-    console.log(error);
-  }else {
-    console.log(results);
-    let label = results[1].label;
-    let conf = results[1].confidence;
-
-    fill(0);
-    textSize(64);
-    text(label, 100, height - 100);
-    createDiv(`Label: ${results[1].label}`);
-    createDiv(`Confidence: ${nf(results[1].confidence, 1, 4)}`);
-  }
-}
+// Render the fastest one first, then following on
+Promise.race([
+  recordVideoOne,
+  recordVideoTwo,
+  recordVideoThree
+]).then(message => { console.log(message); });
+//------------------------------------------------------------------
